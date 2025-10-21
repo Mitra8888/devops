e2c219679eaed6805b51d574b48970cef1a1ab32
Uros Jovanovic <urosjovanovi7@gmail.com>
2025-10-21 11:56:48 +0200

    workflow

--- backend/app.js
+++ backend/app.js
@@ -18,21 +18,31 @@ app.get('/', (req, res) => {
     res.send('Welcome to Customers API!');
 });
 
-app.listen(port,'0.0.0.0', (error) => {
-    if (!error)
-        console.log("Server is Successfully Running, and App is listening on port " + port);
-    else
-        console.log("Error occurred, server can't start", error);
-});
+app.get('/health', (req, res)=>{
+    const dbReady = mongoose.connection.readyState ===1;
+    res.status(dbReady ? 200: 500).json({ mongoConnected:dbReady});
+})
 
-main().catch(err => console.log(err));
 
-async function main() {
-    const connectionString = process.env.MONGO_URI || "mongodb+srv://mitra:mitra@mitra.rjpyleu.mongodb.net/?retryWrites=true&w=majority&appName=Mitra";
-    
-    mongoose.set('strictQuery', true);
-    await mongoose.connect(connectionString,
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-    console.log("Connected to MongoDB", connectionString);
-}
+async function startServer(){
+    try {
+        const connectionString =
+        process.env.MONGO_URI ||
+        "mongodb+srv://mitra:mitra@mitra.rjpyleu.mongodb.net/?retryWrites=true&w=majority&appName=Mitra";
+
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDb");
+
+        app.listen(port, '0.0.0.0', ()=>{
+            console.log('server is running on port ${port}');
+        });
+    } catch (error){
+        console.errror("failed to connect to mongodb", error);
+        process.exit(1);
+    }
+} 
+startServer();
