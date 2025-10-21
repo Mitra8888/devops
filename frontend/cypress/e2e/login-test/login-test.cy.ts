c72e2ad2f2633265e0206158228c7f090379affa
Uros Jovanovic <urosjovanovi7@gmail.com>
2025-10-21 11:40:53 +0200

    workflow

--- frontend/cypress/e2e/login-test/login-test.cy.ts
+++ frontend/cypress/e2e/login-test/login-test.cy.ts
@@ -12,14 +12,19 @@ describe('login test', () => {
         cy.get('input[formControlName="email"]').type('e2etest@gmail.com')
         cy.get('input[formControlName="phone"]').type('123456')
 
+        cy.wait(2000)
         cy.contains('button', 'Login').click()
 
-        cy.url().should('include','home',{ timeout: 5000 })
+         cy.url().then(url => {
+        cy.log("Current URL: " + url)
+        })
+
+        cy.url().should('include','home',{ timeout: 10000 })
         cy.contains('Welcome Cypress')
 
         cy.contains('button','LogOut').click()
 
-        cy.url().should('include','login',{ timeout: 5000 })
+        cy.url().should('include','login',{ timeout: 10000 })
         cy.contains('Login')
     })
 
