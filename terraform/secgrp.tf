c259290156a2102577aac009b6bbfb78e6898d15
Mitra8888 <urosjovanovi7@gmail.com>
2026-05-23 18:42:45 +0200

    sec group fmt workflow fix

--- terraform/secgrp.tf
+++ terraform/secgrp.tf
@@ -165,11 +165,11 @@ resource "aws_vpc_security_group_ingress_rule" "allow_devops-sg" {
 
 }
 resource "aws_vpc_security_group_ingress_rule" "allow-grafana_to_loki" {
-  security_group_id = aws_security_group.loki-sg.id
+  security_group_id            = aws_security_group.loki-sg.id
   referenced_security_group_id = aws_security_group.grafana-sg.id
-  from_port = 3100
-  ip_protocol = "tcp"
-  to_port = 3100
+  from_port                    = 3100
+  ip_protocol                  = "tcp"
+  to_port                      = 3100
 }
 resource "aws_vpc_security_group_egress_rule" "loki_allowoOutBoundIPV4" {
   security_group_id = aws_security_group.loki-sg.id
